566d41f85876dd76f41525900ced9e3a64716b5a
Carlos Javier Urango Martinez <Carlos.X.Urango.-ND@disney.com>
2017-10-02 12:22:02 -0500

    adding test for exercise model and controller

--- test/controllers/api/v1/exercises_controller_test.rb
+++ test/controllers/api/v1/exercises_controller_test.rb
@@ -1,19 +1,60 @@
 require 'test_helper'
 
 class Api::V1::ExercisesControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
+  test 'should get progress' do
+    get :progress
     assert_response :success
+    assert_equal '{"progress":0}', @response.body
   end
 
-  test "should get create" do
-    get :create
+  test 'should get exercise' do
+    get :show, id: 1
     assert_response :success
+    assert_equal '{"id":1,'\
+      '"started":false,'\
+      '"finished":false,'\
+      '"category":"spelling",'\
+      '"word":null,'\
+      '"answer":null}', @response.body
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
+  test 'should not get invalid exercise id' do
+    get :show, id: 4
+    assert_response :missing
+    assert_equal '{"message":"Not Found","status":404}', @response.body
   end
 
+  test 'should post exercise' do
+    post :create
+    assert_response :success
+    assert_equal '{"id":3,'\
+      '"started":false,'\
+      '"finished":false,'\
+      '"category":"spelling",'\
+      '"word":{"id":1,'\
+        '"complete_word":"test",'\
+        '"letter_pool":["t","e","s","t"]'\
+      '},'\
+      '"answer":null}', @response.body
+  end
+
+  test 'should not put invalid exercise id' do
+    put :update, { finished: true, answer: { answer_word: 'test', accurate: true }, id: 4 }
+    assert_response :missing
+    assert_equal '{"message":"Not Found","status":404}', @response.body
+  end
+
+  test 'should put exercise' do
+    put :update, { finished: true, answer: { answer_word: 'test', accurate: true }, id: 2 }
+    assert_response :success
+    assert_equal '{"id":2,'\
+      '"started":false,'\
+      '"finished":true,'\
+      '"category":"spelling",'\
+      '"word":null,'\
+      '"answer":{"id":3,'\
+        '"answer_word":"test",'\
+        '"accurate":true'\
+      '}}', @response.body
+  end
 end
--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ExersiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'can be initialized' do
+    exercise = Exercise.new
+    assert_instance_of Exercise, exercise
+  end
+
+  test 'get the next word id' do
+    assert_equal Exercise.next_word, 1
+  end
+
 end
